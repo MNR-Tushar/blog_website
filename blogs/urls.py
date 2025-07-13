53c0bee22310742dfef92f93277f71e33d60e7f6
Md Naimur Rahman <rahman23105101275@diu.edu.bd>
2025-07-14 01:34:49 +0600

    Comment and Reply done

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('category_blogs/<str:slug>',Category_blogs,name='category_blogs'),
     path('tag_blogs/<str:slug>',Tag_blogs,name='tag_blogs'),
     path('blog_details/<str:slug>',blog_details,name='blog_details'),
+    path('add_reply/<int:blog_id>/<int:comment_id>/',add_reply,name='add_reply'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Blog, Category,Tags
+from urllib import request
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Blog, Category, Reply,Tags,Comment
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
+from .forms import TextForm
 
 def home(request):
     blogs=Blog.objects.order_by('-created_date')
@@ -62,12 +64,42 @@ def Tag_blogs(request,slug):
     return render(request,'tag_blogs.html',context)
 
 def blog_details(request,slug):
+    form=TextForm()
     blog=get_object_or_404(Blog,slug=slug)
     category=Category.objects.get(id=blog.category.id)
     related_blogs=category.category_blogs.all()
+
+    if request.method == "POST" and request.user.is_authenticated:
+     form=TextForm(request.POST)
+     if form.is_valid():
+         Comment.objects.create(
+             user=request.user,
+             blog=blog,
+             text=form.cleaned_data.get('text'),
+         )
+         return redirect('blog_details',slug=slug)
+
     context={
         "blog":blog,
-        "related_blogs":related_blogs
+        "related_blogs":related_blogs,
+        "form":form
     }
 
     return render(request,'blog_details.html',context)
+
+def add_reply(request,blog_id,comment_id):
+    
+    if request.method == "POST":
+        form=TextForm(request.POST)
+        if form.is_valid():
+            blog=get_object_or_404(Blog,id=blog_id)
+            comment=get_object_or_404(Comment,id=comment_id)
+            Reply.objects.create(
+        user=request.user,
+        comment=comment,
+        text=form.cleaned_data.get('text')
+    )
+    return redirect('blog_details', slug=blog.slug)
+    
+    
+    
